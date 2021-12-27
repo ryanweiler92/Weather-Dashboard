98da08f53f677a6db569dbab53f33693544f895f
ryanweiler92 <ryanweiler92@gmail.com>
2021-12-26 20:46:23 -0500

    add search results history

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -16,5 +16,14 @@ citySearchBtn.addEventListener("click", function(e) {
 
     console.log('submitting form:', citySearchData);
 
-    
+    var searchHistoryEl = document.createElement("li");
+    searchHistoryList.appendChild(searchHistoryEl);
+
+    var searchHistoryButton = document.createElement("button");
+    searchHistoryButton.classList = 'btn mt-3 city-button';
+    searchHistoryButton.setAttribute('type', "submit");
+    searchHistoryButton.innerHTML = citySearchData.city;
+    searchHistoryEl.appendChild(searchHistoryButton);
+
+
 })
