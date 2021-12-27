def4e65cbe299d1b8ae73a4f4124abf2af2a9e7f
ryanweiler92 <ryanweiler92@gmail.com>
2021-12-26 21:41:37 -0500

    save to local storage

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,25 +5,55 @@ var citySearchBtn = document.getElementById("search-btn")
 //unorder list element for city search history
 var searchHistoryList = document.getElementById("search-history");
 
+//empty object
+var citySearchDataList = []
+
+//variable to assign id's to each city search history
+var cityCount = 0
 
 //get city from search input
 citySearchBtn.addEventListener("click", function(e) {
     e.preventDefault();
 
-    var citySearchData = {
-        city: citySearchInput.value,
-    };
+  
 
-    console.log('submitting form:', citySearchData);
+    console.log('submitting form:', citySearchDataList);
 
+    //create li element
     var searchHistoryEl = document.createElement("li");
     searchHistoryList.appendChild(searchHistoryEl);
 
+     
+    //create button element with city value
     var searchHistoryButton = document.createElement("button");
     searchHistoryButton.classList = 'btn mt-3 city-button';
     searchHistoryButton.setAttribute('type', "submit");
+    searchHistoryButton.setAttribute('id', cityCount ++)
+
+
+    //create object to store data and push to array
+    
+    var citySearchData = {}
+    citySearchData.city = citySearchInput.value;
+    citySearchData.id = searchHistoryButton.id
+    citySearchDataList.push(citySearchData)
+   
+
+
+
     searchHistoryButton.innerHTML = citySearchData.city;
     searchHistoryEl.appendChild(searchHistoryButton);
 
+     
+
+
+    saveCities();
 
 })
+
+var saveCities = function(){
+    for (var i = 0; i <citySearchDataList.length; i++) {
+        
+        localStorage.setItem(citySearchDataList[i].id, citySearchDataList[i].city)
+    }
+}
