0b71cdcce6669b1e7682c055065956c05bef1036
ryanweiler92 <ryanweiler92@gmail.com>
2021-12-27 10:33:34 -0500

    trying to load from local storage

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -15,8 +15,6 @@ var cityCount = 0
 citySearchBtn.addEventListener("click", function(e) {
     e.preventDefault();
 
-  
-
     console.log('submitting form:', citySearchDataList);
 
     //create li element
@@ -32,28 +30,57 @@ citySearchBtn.addEventListener("click", function(e) {
 
 
     //create object to store data and push to array
-    
     var citySearchData = {}
     citySearchData.city = citySearchInput.value;
     citySearchData.id = searchHistoryButton.id
     citySearchDataList.push(citySearchData)
-   
-
-
 
     searchHistoryButton.innerHTML = citySearchData.city;
     searchHistoryEl.appendChild(searchHistoryButton);
 
-     
-
-
+    //run save cities function
     saveCities();
 
 })
 
+
+var loadCities = function() {
+
+
+    for (var i = 0; i <localStorage.length; i++){
+
+        
+        //variable to retrieve the value of each key in local storage
+       var getCity = localStorage.getItem(localStorage.key[i])
+
+       console.log(localStorage.key[i])
+
+
+        var searchHistoryEl = document.createElement("li");
+        searchHistoryList.appendChild(searchHistoryEl);
+
+        var searchHistoryButton = document.createElement("button");
+        searchHistoryButton.classList = 'btn mt-3 city-button';
+        searchHistoryButton.setAttribute('type', "submit");
+        searchHistoryButton.setAttribute('id', localStorage.key)
+
+        searchHistoryButton.innerHTML = getCity;
+        searchHistoryEl.appendChild(searchHistoryButton);
+    }
+}
+
+
+
+//save search results to local storage
 var saveCities = function(){
     for (var i = 0; i <citySearchDataList.length; i++) {
         
-        localStorage.setItem(citySearchDataList[i].id, citySearchDataList[i].city)
+        var cityIds = citySearchDataList[i].id;
+
+        var cityName = citySearchDataList[i].city
+
+        localStorage.setItem(cityIds, cityName)
     }
 }
+
+loadCities();
